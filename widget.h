307dae6c7ae64f4051e86645b173642425a4da5d
slowplayer <gaoxiao7163201@163.com>
2015-02-07 16:41:16 +0800

    Create widget.h

---
+++ widget.h
@@ -0,0 +1,31 @@
+#ifndef WIDGET_H
+#define WIDGET_H
+
+#include <QWidget>
+#include <QNetworkAccessManager>
+
+namespace Ui {
+class Widget;
+}
+
+class Widget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit Widget(QWidget *parent = 0);
+    ~Widget();
+
+public slots:
+    void query(QNetworkReply *reply);
+private slots:
+    void on_goButton_clicked();
+    void showtime();
+private:
+    Ui::Widget *ui;
+    QNetworkAccessManager *mgr;
+    QString *content;
+
+};
+
+#endif // WIDGET_H
